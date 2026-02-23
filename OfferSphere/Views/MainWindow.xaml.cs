9a4f88e78ad72273500a55e64227f7556b3332ad
TauroCrypt <moors.dominik@gmail.com>
2026-02-23 13:38:59 +0100

    Navigierung zu den einzelnen View.

--- OfferSphere/Views/MainWindow.xaml.cs
+++ OfferSphere/Views/MainWindow.xaml.cs
@@ -21,12 +21,36 @@ namespace OfferSphere
         public MainWindow()
         {
             InitializeComponent();
-            // Perform database operations here
         }
 
         private void Menu_Click(object sender, RoutedEventArgs e)
         {
-            //MainFrame.Navigate(new Uri("Views\\CustomersView.xaml", UriKind.Relative));
+            if (sender is Button clickedButton)
+            {
+                if(clickedButton is null)
+                    throw new InvalidOperationException("Der angeklickte Button ist null.");
+
+                string tagWert = clickedButton.Tag.ToString();
+
+                switch (tagWert)
+                {
+                    case "btn_MenuCustomers":
+                        MainFrame.Navigate(new Uri("Views\\CustomersView.xaml", UriKind.Relative));
+                        break;
+                    case "btn_MenuService":
+                        MainFrame.Navigate(new Uri("Views\\ServicesView.xaml", UriKind.Relative));
+                        break;
+                    case "btn_MenuQuote":
+                        MainFrame.Navigate(new Uri("Views\\QuotesView.xaml", UriKind.Relative));
+                        break;
+                    case "btn_MenuInvoice":
+                        MainFrame.Navigate(new Uri("Views\\InvoicesView.xaml", UriKind.Relative));
+                        break;
+                    default:
+                        MessageBox.Show("Unbekannte Aktion");
+                        break;
+                }
+            }
         }
     }
 }
